e4d01f7bfda4e437d7e6c1b182669711acd4efc1
Soundhar <laptopisasset@gmail.com>
2023-10-08 00:37:02 +0530

    Measuring distance between camera and mesh.

--- src/script.js
+++ src/script.js
@@ -20,6 +20,8 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
+console.log(mesh.position.distanceTo(camera.position));
+
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector(".webgl"),
 });
