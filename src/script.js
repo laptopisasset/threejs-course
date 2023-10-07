a81eff20f4a95a6f94f57bd6811d00148e193a02
Soundhar <laptopisasset@gmail.com>
2023-10-08 00:47:53 +0530

    scaling of objects.

--- src/script.js
+++ src/script.js
@@ -6,13 +6,12 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 mesh.position.set(0.7, -0.6, 1);
+mesh.scale.x = 2;
+mesh.scale.y = 0.5;
+mesh.scale.z = 0.5;
 scene.add(mesh);
 
 console.log(mesh.position.length());
-
-// Axes helper
-const axesHelper = new THREE.AxesHelper(3);
-scene.add(axesHelper);
 
 const sizes = {
   width: 800,
@@ -20,8 +19,6 @@ const sizes = {
 };
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-camera.position.y = 1;
-camera.position.x = 1;
 scene.add(camera);
 
 console.log(mesh.position.distanceTo(camera.position));
