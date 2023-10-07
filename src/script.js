94e684af2328ff886e2c0e8cb1c8ad4e4177b606
Soundhar <laptopisasset@gmail.com>
2023-10-08 01:21:38 +0530

    creating seperate groups.

--- src/script.js
+++ src/script.js
@@ -2,6 +2,40 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+cube2.position.x = -2;
+
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+cube3.position.x = 2;
+
+group.add(cube3);
+
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
 const sizes = {
   width: 800,
   height: 600,
@@ -9,8 +43,6 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
-
-console.log(mesh.position.distanceTo(camera.position));
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector(".webgl"),
